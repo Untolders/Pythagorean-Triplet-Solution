6ef4524595a3a66ce011176814b44b4d2fb59248
Anurag Singh <131255604+Untolders@users.noreply.github.com>
2023-11-03 23:46:57 +0530

    Create Pythagorean-Triplet-Solution.cpp

---
+++ Pythagorean-Triplet-Solution.cpp
@@ -0,0 +1,91 @@
+***********************************************************************************  Question  ***********************************************************************************************************
+Given an array arr of n integers, write a function that returns true if there is a triplet (a, b, c) from the array (where a, b, and c are on different indexes) that satisfies a2 + b2 = c2, otherwise return false.
+
+Example 1:
+
+Input:
+N = 5
+Arr[] = {3, 2, 4, 6, 5}
+Output: Yes
+Explanation: a=3, b=4, and c=5 forms a
+pythagorean triplet.
+Example 2:
+
+Input:
+N = 3
+Arr[] = {3, 8, 5}
+Output: No
+Explanation: No such triplet possible.
+Your Task:
+You don't have to take any input or print any thing. You have to complete the function checkTriplet() which takes an array arr, a single integer n, as input parameters and returns boolean denoting answer to the problem.
+Note: The driver will print "Yes" or "No" instead of corresponding to the boolean value returned.
+
+Expected Time Complexity: O(n+max(Arr[i])2)
+Expected Auxiliary Space: O(max(Arr[i]))
+
+Constraints:
+1 <= n <= 105
+1 <= arr[i] <= 1000
+
+
+
+
+
+*********************************************************************************** Solution  ***********************************************************************************************************
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	// Function to check if the
+	// Pythagorean triplet exists or not
+	bool checkTriplet(int arr[], int n) {
+	    // code here
+	      for (int i = 0; i < n; i++) {
+            arr[i] = arr[i] * arr[i];
+        }
+ 
+        unordered_set<int> squaredValues(arr, arr + n);
+ 
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int sumOfSquares = arr[i] + arr[j];
+                if (squaredValues.find(sumOfSquares) != squaredValues.end()) {
+                    return true; 
+                }
+            }
+        }
+ 
+        return false;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.checkTriplet(arr, n);
+        if (ans) {
+            cout << "Yes\n";
+        } else {
+            cout << "No\n";
+        }
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
